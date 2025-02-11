54bae6c5ec8679da7492da86c49a3c13f6e8433f
Lemuel Guballa <lemuel.guballa34@gmail.com>
2025-02-11 21:51:07 +0800

    reworked frontend for stability

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-blue-500">Omni Messenger</h1>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
